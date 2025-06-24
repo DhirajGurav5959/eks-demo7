61176a8a2d5c512cf83d2a1698bb483b5608dc6b
DhirajGurav5959 <dhirajgurav72@gmail.com>
2025-06-24 18:30:48 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,25 +1,28 @@
 terraform {
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
-    organization = "TeKanAid"
-
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "backstage-eks-aws"
-    }
+  backend "s3" {
+    bucket = "shiviiii"  # Replace with your actual bucket
+    key    = "eks-demo/terraform.tfstate"
+    region = "us-west-2"
+    encrypt = true
   }
 }
 
 module "eks" {
-  source  = "app.terraform.io/TeKanAid/eks/aws"
-  version = "0.0.7"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.17.0"
 
-  region               = "us-east-1"
+  cluster_name         = var.clusterName
   cluster_version      = "1.27"
-  cluster_name         = "my_eks_cluster"
-  instance_types       = ["t2.small"]
-  vpc_cidr             = "10.0.0.0/16"
-  cluster_min_size     = 1
-  cluster_max_size     = 3
-  cluster_desired_size = 2
+  subnet_ids           = []
+  vpc_id               = ""
+  instance_types       = ["t3.small"]
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+  }
 }
